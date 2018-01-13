5e4a661bb7cc601ca2eb8d9a9a6dec32fc4e6b49
Rud156 <rudrangshu_1997@yahoo.com>
2018-01-13 11:36:54 +0530

    Added Start Method

---
+++ Assets/Scripts/Camera/CameraLookAt.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float damping = 5f;
+
+    [Header("Target")]
+    public Transform lookAtTarget;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var position = lookAtTarget.position - gameObject.transform.position;
+        var newRotation = Quaternion.LookRotation(position);
+        gameObject.transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, damping * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Camera/CameraRotator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    [Header("Attributes")]
+    public float rotationSpeed = 7;
+    public float rotationTime = 7;
+
+    [Header("Camera Attributes")]
+    public Animator animator;
+
+    private bool rotate = false;
+
+    private void Start()
+    {
+        StartCoroutine(StartRotation());
+    }
+
+    private IEnumerator StartRotation()
+    {
+        yield return new WaitForSeconds(1.99f);
+        rotate = true;
+        yield return new WaitForSeconds(rotationTime);
+        rotate = false;
+        gameObject.transform.rotation = Quaternion.Euler(Vector3.zero);
+        animator.SetBool("ZoomIn", true);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (rotate)
+            gameObject.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
