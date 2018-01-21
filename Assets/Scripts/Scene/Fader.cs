5928833d37eac05914f7cce2989133a44c52aaa6
Rud156 <rudrangshu_1997@yahoo.com>
2018-01-21 22:24:04 +0530

    GameManager

---
+++ Assets/Scripts/Scene/Fader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+
+    [Header("Canvas Elements")]
+    public Image faderImage;
+
+    [Header("Activator")]
+    public Animator cameraAnimator;
+    public CameraRotator cameraRotator;
+
+    [Header("Required Stats")]
+    public float reductionRate = 5;
+    public float startAlpha = 255;
+
+    private float currentReductionRate;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentReductionRate = 0;
+        cameraRotator.enabled = false;
+        cameraAnimator.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentReductionRate >= startAlpha)
+        {
+            cameraAnimator.enabled = true;
+            cameraRotator.enabled = true;
+            gameObject.GetComponent<Fader>().enabled = false;
+            return;
+        }
+
+        faderImage.color = new Color(0, 0, 0, (startAlpha - currentReductionRate) / 255);
+        currentReductionRate += reductionRate * Time.deltaTime;
+    }
+}
