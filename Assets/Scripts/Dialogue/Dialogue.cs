59d5fa5e66b695f614cbb343e95f4a91456a69b8
Rud156 <rudrangshu_1997@yahoo.com>
2018-01-24 08:32:52 +0530

    Updated with dialogue script

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialogue/Basic")]
+public class Dialogue : ScriptableObject
+{
+    public new string name;
+
+    [TextArea]
+    public string[] sentences;
+}
---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("Required Components")]
+    public Text nameText;
+    public Text dialogueText;
+
+    [Header("Dialogue Data")]
+    public Dialogue[] dialogues;
+    public float waitTimeAfterDialogue = 2;
+
+
+    private bool sentenceDisplayCompleted;
+    
+    void Start()
+    {
+        sentenceDisplayCompleted = true;
+        StartCoroutine(StartDialogue());
+    }
+
+    private IEnumerator StartDialogue()
+    {
+        foreach (Dialogue dialogue in dialogues)
+        {
+            nameText.text = dialogue.name;
+            foreach (string line in dialogue.sentences)
+            {
+                StartCoroutine(TypeSentence(line));
+                yield return new WaitUntil(() => sentenceDisplayCompleted);
+            }
+            yield return null;
+        }
+
+        EndDialogue();
+    }
+
+    private IEnumerator TypeSentence(string sentence)
+    {
+        sentenceDisplayCompleted = false;
+        dialogueText.text = "";
+        foreach (var letter in sentence)
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(waitTimeAfterDialogue);
+        sentenceDisplayCompleted = true;
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
+}
