e955ca97ae2fe8484e5651f73806f70cc5242a1b
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-05-08 14:11:18 +0530

    Initial Adding of PowerUps
    TODO:
    
    Add Effect for collecting powerup
    Store data properly

---
+++ Assets/Scripts/Player/IncreaseDashCount.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseDashCount : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (!other.CompareTag("DashCollectible"))
+            return;
+    }
+}
---
+++ Assets/Scripts/Player/IncreaseHealthCount.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseHealthCount : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("HealthCollectible"))
+            return;
+    }
+}
