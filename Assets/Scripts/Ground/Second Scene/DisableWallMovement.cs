76bc56d5c3f76317eb36f863a6f6ab93963cc87f
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-05-19 09:48:11 +0530

    Fixed Text Animations Animations

---
+++ Assets/Scripts/Ground/Second Scene/DisableWallMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableWallMovement : MonoBehaviour
+{
+
+    public Animator wallAnimator;
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        Rigidbody target = other.GetComponent<Rigidbody>();
+        if (!target || !other.CompareTag("Player"))
+            return;
+
+        wallAnimator.Play(UITextConstants.secondScreenWallIdleAnimation);
+    }
+
+}
