3f9dca52b9fff7d9a58fe392d4059bf4b9eeca1f
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-05-22 20:03:37 +0530

    Updated Movement to stop when button is released

--- Assets/Scripts/Player/BasicMovement.cs
+++ Assets/Scripts/Player/BasicMovement.cs
@@ -9,11 +9,13 @@ public class BasicMovement : MonoBehaviour
     public float rotationSpeed = 10f;
 
     private Rigidbody target;
+    private bool playermovementStopped;
 
     // Use this for initialization
     void Start()
     {
         target = gameObject.GetComponent<Rigidbody>();
+        playermovementStopped = false;
     }
 
     // Update is called once per frame
@@ -26,7 +28,16 @@ public class BasicMovement : MonoBehaviour
         target.transform.Rotate(Vector3.up * moveX * rotationSpeed * Time.deltaTime);
 
         float moveZ = Input.GetAxis(ControlConstants.verticalAxis);
-        Vector3 forwardMovement = gameObject.transform.forward * moveZ * movementSpeed * Time.deltaTime;
-        target.AddForce(forwardMovement, ForceMode.VelocityChange);
+        if (moveZ == 0 && !playermovementStopped)
+        {
+            target.velocity = Vector3.zero;
+            playermovementStopped = true;
+        }
+        else if (moveZ != 0)
+        {
+            Vector3 forwardMovement = gameObject.transform.forward * moveZ * movementSpeed * Time.deltaTime;
+            target.AddForce(forwardMovement, ForceMode.VelocityChange);
+            playermovementStopped = false;
+        }
     }
 }
