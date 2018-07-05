e9ae76af9dc5872e64f1683954a7b472d3a457d8
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-05 23:11:19 +0530

    Changes
    - Added Timeline
    - Minor Player Animation

---
+++ Assets/Scripts/Camera/ShakeCameraOnActive.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class ShakeCameraOnActive : MonoBehaviour
+{
+
+    public float magnitude = 4f;
+    public float roughness = 3f;
+    public float fadeInTime = 0.5f;
+    public float fadeOutTime = 5f;
+    public float totalShakeTime = 2f;
+
+
+    private CameraShakeInstance cameraShakeInstance;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(StartShake());
+    }
+
+    IEnumerator StartShake()
+    {
+        cameraShakeInstance = CameraShaker.Instance.StartShake(magnitude, roughness, fadeInTime);
+        yield return new WaitForSeconds(totalShakeTime);
+        cameraShakeInstance.StartFadeOut(fadeOutTime);
+    }
+}
