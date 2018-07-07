238ebdbc42cdbd686a6df92aa83d7dba6d6880ab
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-07 09:52:48 +0530

    Broken Animator Movement

--- Assets/Scripts/Ground/ResetJumpOnCollision.cs
+++ Assets/Scripts/Ground/ResetJumpOnCollision.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResetJumpOnCollision : MonoBehaviour {
+public class ResetJumpOnCollision : MonoBehaviour
+{
 
-	/// <summary>
-	/// OnCollisionEnter is called when this collider/rigidbody has begun
-	/// touching another rigidbody/collider.
-	/// </summary>
-	/// <param name="other">The Collision data associated with this collision.</param>
-	void OnCollisionEnter(Collision other)
-	{
-		GameObject player = other.gameObject;
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        GameObject player = other.gameObject;
         Rigidbody target = player.GetComponent<Rigidbody>();
         if (!target || !player.CompareTag("Player"))
             return;
 
         Core.currentJumpCount = 0;
-	}
+    }
 
 }
--- Assets/Scripts/Player/Data/Core.cs
+++ Assets/Scripts/Player/Data/Core.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class Core : MonoBehaviour
 {
-	public static int maxJumpCount = 2;
-	public static int currentJumpCount = 0;
+    public static int maxJumpCount = 2;
+    public static int currentJumpCount = 0;
 
-	public static int maxHealth = 3;
-	public static float currentHealthLeft = 3;
-	public static int healthPortalsCollected = 0;
-	public static int maxHealthPortalsForIncrease =  2;
+    public static int maxHealth = 3;
+    public static float currentHealthLeft = 3;
+    public static int healthPortalsCollected = 0;
+    public static int maxHealthPortalsForIncrease = 2;
 
-	public static int maxDashes = 3;
-	public static float currentDashesLeft = 3;
-	public static int dashPortalsCollected = 0;
-	public static int maxDashPortalsForIncrease = 2;
-	public static float dashIncreaseRate = 0.01f;
-	public static float dashUseRate = 1f;
+    public static int maxDashes = 3;
+    public static float currentDashesLeft = 3;
+    public static int dashPortalsCollected = 0;
+    public static int maxDashPortalsForIncrease = 2;
+    public static float dashIncreaseRate = 0.01f;
+    public static float dashUseRate = 1f;
 
-	public static bool stopPlayerMovement = false;
-	public static bool wallBreakCollected = true;
-	
-	public static Vector3 currentCheckPoint = new Vector3(3f, 4f, -11f);
+    public static bool stopPlayerMovement = false;
+    public static bool wallBreakCollected = true;
+
+    public static Vector3 currentCheckPoint = new Vector3(-71.95f, 4f, 0.04f);
 }
