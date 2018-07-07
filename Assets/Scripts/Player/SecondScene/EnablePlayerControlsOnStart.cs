fa8ab18d3c00459fc806142f423b19ed0e9cf3fa
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-07 21:42:00 +0530

    Fixed Player Movement

---
+++ Assets/Scripts/Player/SecondScene/EnablePlayerControlsOnStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablePlayerControlsOnStart : MonoBehaviour
+{
+    private Rigidbody target;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = gameObject.GetComponent<Rigidbody>();
+
+        Core.stopPlayerMovement = false;
+
+        target.isKinematic = false;
+        target.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+}
