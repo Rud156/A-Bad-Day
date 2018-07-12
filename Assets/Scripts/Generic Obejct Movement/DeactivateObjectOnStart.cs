a4dcf206d49e4f52d7814612dae3566216b8806f
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-12 06:49:46 +0530

    Inproved Scene Start Animations

---
+++ Assets/Scripts/Generic Obejct Movement/DeactivateObjectOnStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateObjectOnStart : MonoBehaviour {
+
+	public GameObject objectToDeactivate;
+
+	// Use this for initialization
+	void Start () {
+		objectToDeactivate.SetActive(false);
+	}
+}
