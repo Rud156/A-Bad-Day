56c907b90ec641fdba99beceb431581c7e4bac13
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-13 21:10:10 +0530

    Removed Unused Timelines

---
+++ Assets/Scripts/Generic Obejct Movement/ActivateObjectOnStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObjectOnStart : MonoBehaviour {
+
+	public GameObject objectToActivate;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		objectToActivate.SetActive(true);
+	}
+
+}
