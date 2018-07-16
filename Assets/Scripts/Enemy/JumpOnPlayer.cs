f707aecb7afc66c695c5a941a444ffd99b83de7b
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-16 07:02:49 +0530

    Changes
    - Added Enemy Attack Effect
    - Adding Enemy Jump Attack

---
+++ Assets/Scripts/Enemy/JumpOnPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class JumpOnPlayer : MonoBehaviour
+{
+
+    [Header("Player To Target")]
+    public GameObject player;
+
+    [Header("Jump Stats")]
+    public float jumpUpSpeed = 4f;
+
+
+    private Vector3 playerCurrentPosition;
+    private bool startJump;
+    private Rigidbody target;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = gameObject.GetComponent<Rigidbody>();
+        startJump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startJump)
+            return;
+
+
+    }
+
+    void TriggerJump()
+    {
+        if (startJump)
+            return;
+
+        playerCurrentPosition = player.transform.position;
+        startJump = true;
+    }
+}
--- Assets/Scripts/Ground/Second Scene/DestroyObjectOnContact.cs
+++ Assets/Scripts/Ground/Second Scene/DestroyObjectOnContact.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyObjectOnContact : MonoBehaviour {
+public class DestroyObjectOnContact : MonoBehaviour
+{
 
-	/// <summary>
-	/// OnTriggerEnter is called when the Collider other enters the trigger.
-	/// </summary>
-	/// <param name="other">The other Collider involved in this collision.</param>
-	void OnTriggerEnter(Collider other)
-	{
-		Rigidbody target = other.GetComponent<Rigidbody>();
-		if(!target || other.CompareTag("Player"))
-			return;
+    public bool destroyParent = false;
 
-		Destroy(other.gameObject);
-	}
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        Rigidbody target = other.GetComponent<Rigidbody>();
+        if (!target || other.CompareTag("Player"))
+            return;
+
+        if (destroyParent)
+            Destroy(other.transform.parent.gameObject);
+        else
+            Destroy(other.gameObject);
+    }
 
 }
