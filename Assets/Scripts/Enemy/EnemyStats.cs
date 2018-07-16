8c7063d71f7b80640acd977e8e2daae4162e4369
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-16 23:38:58 +0530

    Changes
    - Added Enemy Jump Script
    - Added Enemy Jump Effect
    - Adding Enemy States

---
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+
+    public static int state = 0;
+    public static bool isJumping = false;
+    public float health = 100;
+
+}
--- Assets/Scripts/Enemy/JumpOnPlayer.cs
+++ Assets/Scripts/Enemy/JumpOnPlayer.cs
@@ -10,35 +10,90 @@ public class JumpOnPlayer : MonoBehaviour
     public GameObject player;
 
     [Header("Jump Stats")]
-    public float jumpUpSpeed = 4f;
+    public int minLaunchAngle = 30;
+    public int maxLaunchAngle = 60;
 
+    [Header("Force Stats")]
+    public float radius = 5f;
+    public float power = 10f;
 
-    private Vector3 playerCurrentPosition;
-    private bool startJump;
+    [Header("Effects")]
+    public GameObject landEffect;
+    public float heightBelowEnemyCenter = 1.5f;
+
     private Rigidbody target;
 
     // Use this for initialization
     void Start()
     {
         target = gameObject.GetComponent<Rigidbody>();
-        startJump = false;
+        EnemyStats.isJumping = false;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
     void Update()
     {
-        if (!startJump)
-            return;
-
+        if (Input.GetMouseButtonDown(0))
+            TriggerJump();
 
     }
 
-    void TriggerJump()
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
     {
-        if (startJump)
+
+        if (!EnemyStats.isJumping)
             return;
 
-        playerCurrentPosition = player.transform.position;
-        startJump = true;
+        if (!other.gameObject.CompareTag("LastGround") && !other.gameObject.CompareTag("Player"))
+            return;
+
+        Vector3 explosionPosition = gameObject.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+        foreach (Collider collider in colliders)
+        {
+            Rigidbody rb = collider.GetComponent<Rigidbody>();
+            if (rb != null && collider.gameObject.CompareTag("Player"))
+                rb.AddExplosionForce(power, explosionPosition, radius, 3f, ForceMode.Impulse);
+        }
+
+        Vector3 instantiationPosition = new Vector3(gameObject.transform.position.x,
+           gameObject.transform.position.y - heightBelowEnemyCenter, gameObject.transform.position.z);
+        GameObject effect = Instantiate(landEffect, instantiationPosition,
+            landEffect.transform.rotation);
+        effect.transform.SetParent(gameObject.transform.parent);
+        EnemyStats.isJumping = false;
+    }
+
+    public void TriggerJump()
+    {
+        if (EnemyStats.isJumping)
+            return;
+
+        EnemyStats.isJumping = true;
+
+        int randomAngle = Random.Range(minLaunchAngle, maxLaunchAngle);
+        target.velocity = BallisticVelocity(randomAngle);
+    }
+
+    private Vector3 BallisticVelocity(float launchAngle)
+    {
+        Vector3 dir = player.transform.position - gameObject.transform.position;  // get target direction
+        float h = dir.y;  // get height difference
+        dir.y = 0;  // retain only the horizontal direction
+        float dist = dir.magnitude;  // get horizontal distance
+        float a = launchAngle * Mathf.Deg2Rad;  // convert angle to radians
+        dir.y = dist * Mathf.Tan(a);  // set dir to the elevation angle
+        dist += h / Mathf.Tan(a);  // correct for small height differences
+
+        // Calculate the velocity magnitude
+        float vel = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a));
+        return vel * dir.normalized;
     }
 }
